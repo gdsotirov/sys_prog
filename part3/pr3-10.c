gdsotirov/sys_prog

commit 1e92d3998d886ae26268298cfb122528cd6e563d

Replace sys/types.h with sys/sysmacros.h
Replace sys/types.h with sys/sysmacros.h to avoid deprecation warning for
major and minor macros with GLIBC >= 2.25 and 'implicit declaration' warnings
with GLIBC >= 2.28

References:
* GNU LibC 2.25 Release Notes:
  https://sourceware.org/glibc/wiki/Release/2.25#sys.2BAC8-types.h_and_sys.2BAC8-sysmacros.h
* GNU LibC 2.28 Release Notes (search major or minor):
  https://www.sourceware.org/ml/libc-alpha/2018-08/msg00003.html

part3/pr3-10.c
@@ -4,9 +4,9 @@
  */
 
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 
 struct stat buf;
 

commit 99ec079b5771f21e6e4c9816422ea96f06d2cfe1

Use sys/sysmacros.h for Linux, GLIBC and HP-UX
Using sys/types.h was a regression from 1e92d39 for Linux and GLIBC, so
update the modification from fc50ec4 to use sys/sysmacros.h back.

part3/pr3-10.c
@@ -6,18 +6,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
-    /* Linux, Android, and other systems using GNU C library */
-#if defined(__linux__) || defined(__GLIBC__) || \
+    /* Linux, Android, and other systems using GNU C library or HP-UX*/
+#if defined(__linux__) || defined(__GLIBC__) || defined(hpux)
+#include <sys/sysmacros.h>
     /* FreeBSD, OpenBSD, NetBSD, and DragonFlyBSD */ \
-    defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || \
+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) || defined(__DragonFly__) || \
     /* Mac OS X */ \
     defined(__APPLE__) && defined(__MACH__) || \
     /* AIX, OSF/1, Tru64 Unix */ \
     defined(_AIX) || defined (__osf__)
 #include <sys/types.h>
-#elif defined(hpux)
-/* HP-UX */
-#include <sys/sysmacros.h>
 #elif defined(sun)
 /* Solaris */
 #include <sys/types.h>